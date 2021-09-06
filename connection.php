4eac9908a2223f53d2c0a55f82bf32af0deed86b
JatinBagai <jatin.bagai01@gmail.com>
2021-09-06 22:34:54 +0530

    new

---
+++ connection.php
@@ -0,0 +1,20 @@
+<?php
+
+$DBHOST = 'localhost';
+$DBUSER = 'root';
+$DBPASS = '';
+$DBNAME = 'gallery2';
+$conn;
+
+$conn = mysqli_connect($DBHOST, $DBUSER, $DBPASS, $DBNAME);
+
+
+if($conn)
+{
+
+}
+else{
+    die("No Connection!");
+}
+
+?>
